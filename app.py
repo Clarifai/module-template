04363a8304a0a1556447e1424ba55d2ce71848d9
Matthew Zeiler <zeiler@clarifai.com>
2023-05-01 11:35:08 -0400

    redirect with qp

--- app.py
+++ app.py
@@ -13,7 +13,12 @@ st.markdown("Please select a specific page by adding it to the url")
 
 cols = st.columns(10)
 
+qp = st.experimental_get_query_params()
+qpstr = ""
+for k, v in qp.items():
+  qpstr += "{}={}&".format(k, v[0])
+
 d = os.path.dirname(__file__)
 for fi, f in enumerate(glob.glob(os.path.join(d, "pages", "*.py"))):
   name = os.path.splitext(os.path.basename(f))[0]
-  ClarifaiStreamlitCSS.buttonlink(cols[fi % len(cols)], name, "/" + name)
+  ClarifaiStreamlitCSS.buttonlink(cols[fi % len(cols)], name, "/" + name + '?' + qpstr)
