71acf05c5475ad6383f0c2abe51cc92cfea1741f
Mogith PN <mogith.nirmala@clarifai.com>
2023-10-06 20:44:01 +0530

    updated mtotal in loop

--- pages/first_page.py
+++ pages/first_page.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from clarifai.client.auth.helper import ClarifaiAuthHelper
 from clarifai.client.auth import create_stub
+from clarifai.client.auth.helper import ClarifaiAuthHelper
+from clarifai.client.user import User
 from clarifai.modules.css import ClarifaiStreamlitCSS
 from google.protobuf import json_format, timestamp_pb2
-from clarifai.client.user import User
 
 st.set_page_config(layout="wide")
 ClarifaiStreamlitCSS.insert_default_css(st)
@@ -28,19 +28,19 @@ if submitted:
     st.write("Number of inputs in table will be: {}".format(mtotal))
 
   # Stream inputs from the app. list_inputs give list of dictionaries with inputs and its metadata .
-  input_obj = User(user_id=userDataObject.user_id).app(app_id=userDataObject.app_id).inputs()  
-  all_inputs=input_obj.list_inputs()
+  input_obj = User(user_id=userDataObject.user_id).app(app_id=userDataObject.app_id).inputs()
+  all_inputs = input_obj.list_inputs()
 
-#added "data_url" which gives the url of the input.
+  #added "data_url" which gives the url of the input.
   data = []
-  for inp in all_inputs:
+  for inp in range(mtotal - 1):
     data.append({
-        "id": inp.id,
-        "data_url":inp.data.text.url,
-        "status": inp.status.description,
-        "created_at": timestamp_pb2.Timestamp.ToDatetime(inp.created_at),
-        "modified_at": timestamp_pb2.Timestamp.ToDatetime(inp.modified_at),
-        "metadata": json_format.MessageToDict(inp.data.metadata),
+        "id": all_inputs[inp].id,
+        "data_url": all_inputs[inp].data.image.url,
+        "status": all_inputs[inp].status.description,
+        "created_at": timestamp_pb2.Timestamp.ToDatetime(all_inputs[inp].created_at),
+        "modified_at": timestamp_pb2.Timestamp.ToDatetime(all_inputs[inp].modified_at),
+        "metadata": json_format.MessageToDict(all_inputs[inp].data.metadata),
     })
 
   st.dataframe(data)
