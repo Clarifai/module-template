dd0402cf9c8f22f47d43929dd5e699a1d823910a
mogith-pn <143642606+mogith-pn@users.noreply.github.com>
2023-10-10 15:53:36 +0530

    Migrated functions from utils to SDK (#3)
    * Migrated functions from utils to SDK
    
    * updated mtotal in loop
    
    * added exceptiion block when index is out of range

--- pages/first_page.py
+++ pages/first_page.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from clarifai.auth.helper import ClarifaiAuthHelper
-from clarifai.client import create_stub
-from clarifai.listing.lister import ClarifaiResourceLister
+from clarifai.client.auth import create_stub
+from clarifai.client.auth.helper import ClarifaiAuthHelper
+from clarifai.client.user import User
 from clarifai.modules.css import ClarifaiStreamlitCSS
 from google.protobuf import json_format, timestamp_pb2
 
@@ -12,12 +12,12 @@ ClarifaiStreamlitCSS.insert_default_css(st)
 auth = ClarifaiAuthHelper.from_streamlit(st)
 stub = create_stub(auth)
 userDataObject = auth.get_user_app_id_proto()
-lister = ClarifaiResourceLister(stub, auth.user_id, auth.app_id, page_size=16)
+
 st.title("Simple example to list inputs")
 
 with st.form(key="data-inputs"):
   mtotal = st.number_input(
-      "Select number of inputs to view in a table:", min_value=10, max_value=100)
+      "Select number of inputs to view in a table:", min_value=5, max_value=100)
   submitted = st.form_submit_button('Submit')
 
 if submitted:
@@ -27,23 +27,27 @@ if submitted:
   else:
     st.write("Number of inputs in table will be: {}".format(mtotal))
 
-  # Stream inputs from the app
-  all_inputs = []
-  for inp in lister.inputs_generator():
-    all_inputs.append(inp)
+  # Stream inputs from the app. list_inputs give list of dictionaries with inputs and its metadata .
+  input_obj = User(user_id=userDataObject.user_id).app(app_id=userDataObject.app_id).inputs()
+  all_inputs = input_obj.list_inputs()
 
-    if len(all_inputs) >= mtotal:
-      break
+  #Check for no of inputs in the app and compare it with no of inputs to be displayed.
+  if len(all_inputs) < (mtotal):
+    raise Exception(
+        f"No of inputs is less than {mtotal}. Please add more inputs or reduce the inputs to be displayed !"
+    )
 
-  # Get some common stuff out of the inputs.
-  data = []
-  for inp in all_inputs:
-    data.append({
-        "id": inp.id,
-        "status": inp.status.description,
-        "created_at": timestamp_pb2.Timestamp.ToDatetime(inp.created_at),
-        "modified_at": timestamp_pb2.Timestamp.ToDatetime(inp.modified_at),
-        "metadata": json_format.MessageToDict(inp.data.metadata),
-    })
+  else:
+    data = []
+    #added "data_url" which gives the url of the input.
+    for inp in range(mtotal):
+      data.append({
+          "id": all_inputs[inp].id,
+          "data_url": all_inputs[inp].data.image.url,
+          "status": all_inputs[inp].status.description,
+          "created_at": timestamp_pb2.Timestamp.ToDatetime(all_inputs[inp].created_at),
+          "modified_at": timestamp_pb2.Timestamp.ToDatetime(all_inputs[inp].modified_at),
+          "metadata": json_format.MessageToDict(all_inputs[inp].data.metadata),
+      })
 
   st.dataframe(data)
