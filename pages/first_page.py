f815966b9a493dab4def10af77176fb3d2935878
Mogith PN <mogith.nirmala@clarifai.com>
2023-12-19 13:48:27 +0530

    modified clarifai version

--- pages/first_page.py
+++ pages/first_page.py
@@ -29,10 +29,10 @@ if submitted:
 
   # Stream inputs from the app. list_inputs give list of dictionaries with inputs and its metadata .
   input_obj = User(user_id=userDataObject.user_id).app(app_id=userDataObject.app_id).inputs()
-  all_inputs = input_obj.list_inputs()
+  all_inputs = list(input_obj.list_inputs())
 
   #Check for no of inputs in the app and compare it with no of inputs to be displayed.
-  if len(all_inputs) < (mtotal):
+  if len((all_inputs)) < (mtotal):
     raise Exception(
         f"No of inputs is less than {mtotal}. Please add more inputs or reduce the inputs to be displayed !"
     )
@@ -41,9 +41,12 @@ if submitted:
     data = []
     #added "data_url" which gives the url of the input.
     for inp in range(mtotal):
+      data_url = all_inputs[inp].data.image.url
+      if len(data_url) < 1:
+        data_url= all_inputs[inp].data.text.url
       data.append({
           "id": all_inputs[inp].id,
-          "data_url": all_inputs[inp].data.image.url,
+          "data_url": data_url,
           "status": all_inputs[inp].status.description,
           "created_at": timestamp_pb2.Timestamp.ToDatetime(all_inputs[inp].created_at),
           "modified_at": timestamp_pb2.Timestamp.ToDatetime(all_inputs[inp].modified_at),
